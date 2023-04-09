7e1aa8cd7825091c8a9aa40b5f47352049e3ecf0
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-09 22:20:48 +0500

    Initial Commit

---
+++ ImageSwiftUI/ImageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSwiftUIApp.swift
+//  ImageSwiftUI
+//
+//  Created by sss on 09.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ImageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
