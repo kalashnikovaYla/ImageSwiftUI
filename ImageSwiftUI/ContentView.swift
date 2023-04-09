618afe0e56fa0ffd0b0b1abb3b0bfdcd2ebe7609
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-09 23:23:56 +0500

    Initial

--- ImageSwiftUI/ContentView.swift
+++ ImageSwiftUI/ContentView.swift
@@ -8,14 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let image = UIImage(named: "image1") ?? UIImage()
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            backGroundImageView
+            logoImageView
+            
+            Text("Converse")
+                .foregroundColor(.white)
+                .fontWeight(.heavy)
+                .font(.largeTitle)
+            
+            asyncImageView
+            
+            Spacer()
         }
-        .padding()
+        .frame(width: UIScreen.main.bounds.width)
+        .background(
+            VStack {
+                LinearGradient(colors: [Color.black, Color.green.opacity(0.7)], startPoint: .top, endPoint: .bottom)
+            }.ignoresSafeArea(.all, edges: .all)
+        )
+    }
+    
+    var logoImageView: some View {
+        Image(uiImage: image)
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 300, height: 300)
+            .clipShape(Circle())
+            .overlay {
+                Circle()
+                    .stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+            
+    }
+    
+    var backGroundImageView: some View {
+        Image("image")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 400, height: 0)
+            .ignoresSafeArea()
+    }
+    
+    var asyncImageView: some View {
+        
+        AsyncImage(url: URL(string: "https://picsum.photos/401")) { phase in
+            
+            switch phase {
+            case .empty:
+                ProgressView()
+                    .accentColor(.white)
+            case .success(let image):
+                image
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:100, height: 100)
+                    .cornerRadius(20)
+            case .failure(let error):
+                VStack {
+                    Image(systemName: "questionmark")
+                    Text(error.localizedDescription)
+                        .font(.headline)
+                }
+            @unknown default:
+                fatalError()
+            }
+        }
+        
+
     }
 }
 
